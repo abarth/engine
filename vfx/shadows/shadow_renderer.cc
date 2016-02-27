223c984ac837bcf2e303aaedc7e2076df5363bae
Adam Barth <abarth@chromium.org>
2016-02-27 00:27:50 -0800

    Stencil positive and negative at the same time

--- vfx/shadows/shadow_renderer.cc
+++ vfx/shadows/shadow_renderer.cc
@@ -47,66 +47,57 @@ void ShadowRenderer::Draw(const gfx::Size size) {
   if (frame_buffer_.is_null())
     frame_buffer_ = FrameBuffer(size);
 
-  {
-    gfx::ScopedFrameBufferBinder bind(frame_buffer_.id());
+  // {
+    // gfx::ScopedFrameBufferBinder bind(frame_buffer_.id());
 
     glClearColor(0.5f, 0.0f, 0.0f, 1.0f);
     glEnable(GL_DEPTH_TEST);
     glViewport(0, 0, size.width(), size.height());
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
     float aspect = static_cast<GLfloat>(size.width()) / static_cast<GLfloat>(size.height());
     Matrix transform;
     transform.SetPerspective(60.0f, aspect, 1.0f, 20.0f );
     transform.PreTranslate(Offset(0, 0, -2));
     color_program_->Draw(transform, geometry_);
-  }
+  // }
 
-  glClearColor(0.0f, 0.0f, 0.5f, 1.0f);
-  glEnable(GL_DEPTH_TEST);
-  glViewport(0, 0, size.width(), size.height());
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-  texture_program_->Draw(Matrix(), frame_buffer_.color().id(), screen_);
+  // glClearColor(0.0f, 0.0f, 0.5f, 1.0f);
+  // glEnable(GL_DEPTH_TEST);
+  // glViewport(0, 0, size.width(), size.height());
+  // glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  // texture_program_->Draw(Matrix(), frame_buffer_.color().id(), screen_);
 
   // Shadows volumes
 
-  // glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE);
-  // glDepthMask(GL_FALSE);
-  // glEnable(GL_CULL_FACE);
-  // glEnable(GL_STENCIL_TEST);
-  // glEnable(GL_POLYGON_OFFSET_FILL);
-  // glPolygonOffset(0.0f, 100.0f);
+  glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE);
+  glDepthMask(GL_FALSE);
+  glEnable(GL_STENCIL_TEST);
+  glEnable(GL_POLYGON_OFFSET_FILL);
+  glPolygonOffset(0.0f, 100.0f);
 
-  // glCullFace(GL_FRONT);
-  // glStencilFunc(GL_ALWAYS, 0x0, 0xff);
-  // glStencilOp(GL_KEEP, GL_INCR, GL_KEEP);
+  glStencilFunc(GL_ALWAYS, 0x0, 0xff);
+  glStencilOpSeparate(GL_BACK, GL_KEEP, GL_INCR_WRAP, GL_KEEP);
+  glStencilOpSeparate(GL_FRONT, GL_KEEP, GL_DECR_WRAP, GL_KEEP);
+  color_program_->Draw(transform, shadow_);
 
-  // color_program_->Draw(transform, shadow_);
+  glDisable(GL_POLYGON_OFFSET_FILL);
+  glDisable(GL_CULL_FACE);
+  glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
 
-  // glCullFace(GL_BACK);
-  // glStencilFunc(GL_ALWAYS, 0x0, 0xff);
-  // glStencilOp(GL_KEEP, GL_DECR, GL_KEEP);
-
-  // color_program_->Draw(transform, shadow_);
-
-  // glDisable(GL_POLYGON_OFFSET_FILL);
-  // glDisable(GL_CULL_FACE);
-  // glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
-
-  // // Shadows.
+  // Shadows.
 
   // glDepthMask(GL_FALSE);
 
-  // glStencilFunc(GL_NOTEQUAL, 0x0, 0xff);
-  // glStencilOp(GL_REPLACE, GL_REPLACE, GL_REPLACE);
+  glStencilFunc(GL_NOTEQUAL, 0x0, 0xff);
+  glStencilOp(GL_REPLACE, GL_REPLACE, GL_REPLACE);
 
-  // glDisable(GL_DEPTH_TEST);
-  // color_program_->Draw(Matrix(), shadow_mask_);
-  // glEnable(GL_DEPTH_TEST);
+  glDisable(GL_DEPTH_TEST);
+  color_program_->Draw(Matrix(), shadow_mask_);
+  glEnable(GL_DEPTH_TEST);
 
-  // glDisable(GL_STENCIL_TEST);
-  // glDepthMask(GL_TRUE);
+  glDisable(GL_STENCIL_TEST);
+  glDepthMask(GL_TRUE);
 
 }
 
